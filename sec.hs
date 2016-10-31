a589fd631f962f890c44d68ac32ba90815d17d65
Noah_Orberg <noah.orberg.00@gmail.com>
2016-10-31 13:53:39 +0900

    add gcdEx

--- sec.hs
+++ sec.hs
@@ -1,5 +1,11 @@
-is_x :: Int -> Int -> Int -> Int
-is_x a y z = ([x | x <- [a,a*2..a*(y-1)] , (mod x y) == z] !! 0) `div` a
+-- p = ??
+-- q = ??
+n = p * q
+w = (p - 1) * (q - 1)
+-- e = ??
+is_x :: Integer -> Integer -> Integer -> [Integer]
+-- is_x a y z = ([x | x <- [a,a*2..a*(y-1)] , (mod x y) == z] !! 0) `div` a
+is_x a y z = [x | x <- [a,a*2..a*(y-1)] , (mod x y) == z]
 
 create_prime_list :: Int -> Int -> [Int]
 create_prime_list n x
@@ -14,4 +20,11 @@ isPrime n = judge_prime_num n (n-1)
         judge_prime_num x y = if (x `mod` y) == 0 then False else judge_prime_num x ( y - 1 )
 
 prime_list :: [Int]
-prime_list = [ x| x <- [7000000000000000000..], and [x `mod` y /= 0 | y <- [2..(floor $ sqrt $ realToFrac x)]]]
+prime_list = [ x| x <- [2..], and [x `mod` y /= 0 | y <- [2..(floor $ sqrt $ realToFrac x)]]]
+
+euclidEx a b  = f a b 1 0 0 1
+  where f r 0  a  _  b  _ = ((a, b), r)
+        f x y a0 a1 b0 b1 = f y r a1 (a0-q*a1) b1 (b0-q*b1)
+          where (q, r) = divMod x y
+
+
